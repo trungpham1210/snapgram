cdc5c4b7102da0b8d1b49d14dd5eb9c16eacddd1
trungpham1210 <77516675+trungpham1210@users.noreply.github.com>
2024-09-14 20:31:28 -0500

    Add files via upload

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,20 @@
+import { z } from "zod"
+
+export const SignupValidation = z.object({
+    name: z.string().min(2,{message: 'Name is too short, must be at least 2 characters.'}),
+    username: z.string().min(2, {message: 'Username is too short, please put at least 2 characters.'}),
+    email: z.string().email(),
+    password: z.string().min(8, {message: 'Password must be at least 8 characters.'}),
+  })
+
+export const SigninValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {message: 'Password must be at least 8 characters.'}),
+})
+
+export const PostValidation = z.object({
+  caption: z.string().min(5).max(2200),
+  file: z.custom<File[]>(),
+  location:z.string().min(2).max(100),
+  tags: z.string(),
+})
